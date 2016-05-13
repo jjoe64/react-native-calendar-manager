fcc509950c088d4ea8dfd4c2e4458b07f5b623a4
Vladimir Adamic <vladimir@shoutem.com>
2016-05-13 10:50:36 +0200

    Add rnpm postlink hook
    - the hook executes a script adding the main project containing the
      AppDelegate to the CalendarManager project's header search path

---
+++ scripts/rnpm-postlink.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var xcode = require('xcode');
+var rnpm = require('rnpm/src/config');
+
+var modulePath = './node_modules/react-native-calendar-manager/CalendarManager.xcodeproj/project.pbxproj';
+var moduleProject = xcode.project(modulePath);
+
+var config = rnpm.getProjectConfig();
+
+if (config.ios && config.ios.projectName) {
+  var projectNameParts = config.ios.projectName.split('.');
+  var projectName;
+
+  if (projectNameParts && projectNameParts.length) {
+    projectName = projectNameParts[0];
+
+    moduleProject.parse((data) => {
+      moduleProject.addToHeaderSearchPaths(`"$(SRCROOT)/../../ios/${projectName}"`);
+      fs.writeFileSync(modulePath, moduleProject.writeSync());
+      console.log(`iOS: Added ${projectName} to ${modulePath} header search path`);
+    });
+  }
+}
