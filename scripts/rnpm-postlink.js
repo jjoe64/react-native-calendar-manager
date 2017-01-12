9d5c0c1b224b90db1e78129d293c71bb131f8a1c
Željko Rumenjak <zeljko@shoutem.com>
2017-01-12 21:39:48 +0000

    Remove unnecessary platform check form postlink script

--- scripts/rnpm-postlink.js
+++ scripts/rnpm-postlink.js
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log(process.platform);
-if (process.platform === 'darwin' && !/^win/.test(process.platform)) {
-  var fs = require('fs');
-  var xcode = require('xcode');
-  var rnpm = require('rnpm/src/config');
+var fs = require('fs');
+var xcode = require('xcode');
+var rnpm = require('rnpm/src/config');
 
-  var modulePath = './node_modules/react-native-calendar-manager/CalendarManager.xcodeproj/project.pbxproj';
-  var moduleProject = xcode.project(modulePath);
+var modulePath = './node_modules/react-native-calendar-manager/CalendarManager.xcodeproj/project.pbxproj';
+var moduleProject = xcode.project(modulePath);
 
-  var config = rnpm.getProjectConfig();
+var config = rnpm.getProjectConfig();
 
-  if (config.ios && config.ios.projectName) {
-    var projectNameParts = config.ios.projectName.split('.');
-    var projectName;
+if (config.ios && config.ios.projectName) {
+  var projectNameParts = config.ios.projectName.split('.');
+  var projectName;
 
-    if (projectNameParts && projectNameParts.length) {
-      projectName = projectNameParts[0];
+  if (projectNameParts && projectNameParts.length) {
+    projectName = projectNameParts[0];
 
-      moduleProject.parse((data) => {
-        moduleProject.addToHeaderSearchPaths(`"$(SRCROOT)/../../ios/${projectName}"`);
-        fs.writeFileSync(modulePath, moduleProject.writeSync());
-        console.log(`iOS: Added ${projectName} to ${modulePath} header search path`);
-      });
-    }
+    moduleProject.parse((data) => {
+      moduleProject.addToHeaderSearchPaths(`"$(SRCROOT)/../../ios/${projectName}"`);
+      fs.writeFileSync(modulePath, moduleProject.writeSync());
+      console.log(`iOS: Added ${projectName} to ${modulePath} header search path`);
+    });
   }
 }
